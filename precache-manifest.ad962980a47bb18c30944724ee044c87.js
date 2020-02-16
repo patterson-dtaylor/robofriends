fa0f476b2da616687156dd8eac3e82fdd0945f28
patterson-dtaylor <patterson.dtaylor@gmail.com>
2020-02-16 12:24:50 -0500

    Updates

---
+++ precache-manifest.ad962980a47bb18c30944724ee044c87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce52887a32d97420520dd275f9a6033",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0f4c6dcd996f8fbe3ec9",
+    "url": "/robofriends/static/css/main.47531ade.chunk.css"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/robofriends/static/js/2.a5747d58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robofriends/static/js/2.a5747d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f4c6dcd996f8fbe3ec9",
+    "url": "/robofriends/static/js/main.02aa19ea.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
