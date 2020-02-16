84f6d2bf4f5902922b1b89fda3324e33899d6982
patterson-dtaylor <patterson.dtaylor@gmail.com>
2020-02-16 12:18:37 -0500

    Updated build to run as a git page

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Taylor Patterson',
+    username: 'TayTay',
+    email: 'patterson.dtaylor@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Carrie Patterson',
+    username: 'CarrieBeth',
+    email: 'carrie_elizabeth@live.com'
+  },
+  {
+    id: 3,
+    name: 'Jackson Dean',
+    username: 'JDean',
+    email: 'jckson.dean@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Jeffery Haineault',
+    username: 'Jefferson',
+    email: 'jhaineault1@googlemail.com'
+  },
+  {
+    id: 5,
+    name: 'Dennis Rothfuss',
+    username: 'ennui',
+    email: 'drothfuss@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Joe Martin',
+    username: 'HunGjoe',
+    email: 'jmartin@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Bo Gatchell',
+    username: 'bo.gat',
+    email: 'bgatchell@gmail.com'
+  },
+  {
+    id: 8,
+    name: 'Trish Gatchell',
+    username: 'trishaaaa',
+    email: 't.gatchole@gmail.com'
+  },
+  {
+    id: 9,
+    name: 'MaeBeth Dean',
+    username: 'mae.dean',
+    email: 'mae.dean@gmail.com'
+  },
+  {
+    id: 10,
+    name: 'Derek Schaper',
+    username: 'ya.boi.D',
+    email: 'derekschaper@gmail.com'
+  }
+];
